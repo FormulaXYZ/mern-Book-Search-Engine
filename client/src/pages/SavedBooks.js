8d3ea2dcf7c4077404bc71fb4736bce314674c6f
FormulaXYZ <thomas.assefa6@gmail.com>
2022-11-13 22:10:22 -0800

    npm inastaled and resolverss.js & typeDefs.js are done

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -32,61 +32,61 @@ const SavedBooks = () => {
       const { data } = await removeBOOK({
         variables: { book },
       });
+      removeBookId(data.removeBOOK.bookId);
     } catch (err) {
       console.error(err);
     }
   };
 
-  if (!books.length) {
-    return <h3>No books Yet</h3>;
-  }
+  // if (!books.length) {
+  //   return <h3>No books Yet</h3>;
+  // }
 
   // upon success, remove book's id from localStorage
-  removeBookId(bookId);
-} catch (err) {
-  console.error(err);
-}
-  };
 
-// if data isn't here yet, say so
-if (!userDataLength) {
-  return <h2>LOADING...</h2>;
-}
-const savedBookIds = userData.savedBooks.map((book) => book.bookId);
-saveBookIds(savedBookIds);
-return (
-  <>
-    <Jumbotron fluid className='text-light bg-dark'>
+  // } catch (err) {
+  //   console.error(err);
+  // }
+  // };
+
+  // if data isn't here yet, say so
+  // if (!userDataLength) {
+  //   return <h2>LOADING...</h2>;
+  // }
+
+  return (
+    <>
+      <Jumbotron fluid className='text-light bg-dark'>
+        <Container>
+          <h1>Viewing saved books!</h1>
+        </Container>
+      </Jumbotron>
       <Container>
-        <h1>Viewing saved books!</h1>
+        <h2>
+          {userData?.savedBooks?.length
+            ? `Viewing ${userData.savedBooks.length} saved ${userData.savedBooks.length === 1 ? 'book' : 'books'}:`
+            : 'You have no saved books!'}
+        </h2>
+        <CardColumns>
+          {userData?.savedBooks?.map((book) => {
+            return (
+              <Card key={book.bookId} border='dark'>
+                {book.image ? <Card.Img src={book.image} alt={`The cover for ${book.title}`} variant='top' /> : null}
+                <Card.Body>
+                  <Card.Title>{book.title}</Card.Title>
+                  <p className='small'>Authors: {book.authors}</p>
+                  <Card.Text>{book.description}</Card.Text>
+                  <Button className='btn-block btn-danger' onClick={() => handleRemoveBook(book.bookId)}>
+                    Delete this Book!
+                  </Button>
+                </Card.Body>
+              </Card>
+            );
+          })}
+        </CardColumns>
       </Container>
-    </Jumbotron>
-    <Container>
-      <h2>
-        {userData.savedBooks.length
-          ? `Viewing ${userData.savedBooks.length} saved ${userData.savedBooks.length === 1 ? 'book' : 'books'}:`
-          : 'You have no saved books!'}
-      </h2>
-      <CardColumns>
-        {userData.savedBooks.map((book) => {
-          return (
-            <Card key={book.bookId} border='dark'>
-              {book.image ? <Card.Img src={book.image} alt={`The cover for ${book.title}`} variant='top' /> : null}
-              <Card.Body>
-                <Card.Title>{book.title}</Card.Title>
-                <p className='small'>Authors: {book.authors}</p>
-                <Card.Text>{book.description}</Card.Text>
-                <Button className='btn-block btn-danger' onClick={() => handleRemoveBook(book.bookId)}>
-                  Delete this Book!
-                </Button>
-              </Card.Body>
-            </Card>
-          );
-        })}
-      </CardColumns>
-    </Container>
-  </>
-);
+    </>
+  );
 };
 
 export default SavedBooks;
